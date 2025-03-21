b1bb5c0c314e8468d9e04fc8792cc9793241a2c3
Jomon Chacko <mailjomonchacko@gmail.com>
2025-03-21 13:24:04 +0530

    1.0

--- api/run-script.py
+++ api/run-script.py
@@ -1,16 +1,17 @@
-from flask import Flask, jsonify
 import subprocess
+from http.server import BaseHTTPRequestHandler
 
-app = Flask(__name__)
-
-@app.route('/run-script', methods=['GET'])
-def run_script():
-    try:
-        # Run the main.py script and capture the output
-        result = subprocess.run(['python', 'main.py'], capture_output=True, text=True)
-        return jsonify({'output': result.stdout, 'error': result.stderr})
-    except Exception as e:
-        return jsonify({'error': str(e)})
-
-if __name__ == '__main__':
-    app.run(debug=True) 
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        try:
+            # Run the main.py script and capture the output
+            result = subprocess.run(['python', 'main.py'], capture_output=True, text=True)
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(bytes(result.stdout, 'utf-8'))
+        except Exception as e:
+            self.send_response(500)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(bytes(str(e), 'utf-8')) 
